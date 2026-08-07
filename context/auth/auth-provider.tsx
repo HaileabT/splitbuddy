c1a37dd47a73c15f596a9685b48a2238c2adb23d
haileab tesfaye <haileabtesfaye11@gmail.com>
2026-08-07 16:20:10 +0300

    fix: fixed on initial loading

--- context/auth/auth-provider.tsx
+++ context/auth/auth-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { getAuth } from "@/lib/client/supabase/auth";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { AuthContext } from "./auth-context";
 import { useSearchParams } from "next/navigation";
@@ -15,15 +15,12 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
 
   const supabase = getAuth();
 
-  const { data: account, refetch: refetchAccount } = accountsClient.useAccountByEmail(user?.email || "doesntexist", true);
-
-  useEffect(() => {
-    reloadAccount();
-  }, [user]);
+  const { data: account, refetch: refetchAccount } = accountsClient.useAccountByEmail(user?.email || "", true);
 
   const reloadAccount = async () => {
     await refetchAccount();
@@ -31,16 +28,12 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     const checkSession = async () => {
-      let token_hash = searchParams.get("token_hash");
-      if (!token_hash && typeof window !== "undefined") {
-        token_hash = window.location.hash;
-      }
       const res = await supabase.auth.getSession();
       const { data: session } = res;
       if (session.session) {
         setUser(session.session.user);
       } else {
-        if (!PUBLIC_PATHS.some((path) => window.location.pathname === path || (path !== "/" && window.location.pathname.startsWith(path)))) {
+        if (!PUBLIC_PATHS.some((path) => path === pathname || (path !== "/" && pathname.startsWith(path)))) {
           router.push("/landing");
         }
       }
@@ -54,7 +47,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     if (session.session) {
       setUser(session.session.user);
     } else {
-      if (!PUBLIC_PATHS.some((path) => window.location.pathname === path || (path !== "/" && window.location.pathname.startsWith(path)))) {
+      if (!PUBLIC_PATHS.some((path) => path === pathname || (path !== "/" && pathname.startsWith(path)))) {
         router.push("/landing");
       }
     }
